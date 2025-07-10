3c6f93eb5958de940e8189b098401dc15950589c
Osiris Peña <osiris.pena@conuco.do>
2025-07-10 16:29:56 -0400

    corrigiendo bug

--- packages/mars-theme/src/processors/colors.js
+++ packages/mars-theme/src/processors/colors.js
@@ -1,33 +1,30 @@
-import {css, connect} from "frontity";
+import { css } from "frontity";
 
 const Colors = {
     name: "Colors",
     priority: 20,
-    test: ({node}) =>
+    test: ({ node }) =>
         node.type === "element",
-    processor: ({node,state}) =>{
+    processor: ({ node, state }) => {
         // Change color backgrounds
 
-        if(node.props?.className?.split(" ").includes("has-background")){
-            if(node.props?.className?.split(" ").includes("has-ccp-primary-background-color")) node.props.css = css`background: ${state.theme.colors.primary};`;
-    
-            else if(node.props?.className?.split(" ").includes("has-ccp-primary-dark-background-color")) node.props.css = css`background: ${state.theme.colors.primary_dark};`;
-    
-            else if(node.props?.className?.split(" ").includes("has-ccp-white-background-color")) node.props.css = css`background: ${state.theme.colors.white};`;
+        if (node.props?.className?.split(" ").includes("has-background")) {
+            if (node.props?.className?.split(" ").includes("has-ccp-primary-background-color")) node.props.css = css`background: ${state.theme.colors.primary};`;
+
+            else if (node.props?.className?.split(" ").includes("has-ccp-primary-dark-background-color")) node.props.css = css`background: ${state.theme.colors.primary_dark};`;
+
+            else if (node.props?.className?.split(" ").includes("has-ccp-white-background-color")) node.props.css = css`background: ${state.theme.colors.white};`;
         }
-        
-        
+
         // Text color
-        if(node.props?.className?.split(" ").includes("has-text-color")){
-            if(node.props?.className?.split(" ").includes("has-ccp-white-color")){
+        if (node.props?.className?.split(" ").includes("has-text-color")) {
+            if (node.props?.className?.split(" ").includes("has-ccp-white-color")) {
                 node.props.css = css`color: ${state.theme.colors.white};`;
             }
         }
 
         return node;
     },
-
 };
 
-
-export default connect(Colors);
+export default Colors;
